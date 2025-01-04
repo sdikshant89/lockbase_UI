9359da74aeeff4aa7f09290c2b9fd84561ea3a4c
Dikshant Sharma <sdikshant89@gmail.com>
2025-01-04 15:39:00 -0700

    Added home page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Lockbase UI</h1>
+      <div className="h-full w-full bg-gradient-to-br from-indigo-800 to-orange-800 via-slate-950 flex justify-center items-center">
+        <div
+          className="text-black text-9xl font-extrabold -rotate-45 scale-150"
+          style={{ fontStretch: 'extra-expanded' }}
+        >
+          <h1>Lockbase</h1>
+          <h1>ɘƨɒdʞɔo⅃</h1>
+        </div>
+      </div>
     </>
   );
 }
