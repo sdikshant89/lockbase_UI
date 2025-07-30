e56a26385ce78da01d71f258599f61f5f601a050
sdikshant89 <sdikshant89@gmail.com>
2025-07-29 23:29:29 -0600

    updated auth type.

--- src/types/userAuthTypes.ts
+++ src/types/userAuthTypes.ts
@@ -1,16 +1,24 @@
 interface AuthState {
-    user: User | null;
-    token?: string;
-    isAuthenticated: boolean;
+  user: User | null;
+  token?: string;
+  isAuthenticated: boolean;
 }
-  
+
+interface SecurityQuestion {
+  id?: number;
+  question: string;
+  answer?: string;
+}
+
 interface User {
-    id?: number;
-    username?: string;
-    email: string;
-    password: string;
-    countryCode?: string;
-    phoneNumber?: number;
+  id?: number;
+  username?: string;
+  email: string;
+  // Do we need to store the password or no?
+  password: string;
+  securityQuestions?: SecurityQuestion[];
+  countryCode?: string;
+  phoneNumber?: number;
 }
 
 export type { AuthState, User };
